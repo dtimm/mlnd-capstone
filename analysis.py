6f9844c0a7ec607393f3f7ec1927901af25d8f19
David Timm <timmmeister@gmail.com>
2016-06-17 16:09:03 -0600

    Leaving TF on branch. Back to DTR.

--- analysis.py
+++ analysis.py
@@ -2,37 +2,23 @@ import pandas as pd
 import numpy as np
 import sklearn.metrics as sk
 
-from sklearn.cross_validation import train_test_split, KFold
+from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
-
-import tensorflow as tf
-
-# Create model
-def mlp(x, weights, biases):
-    # Hidden layer 1, relu activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-    # Hidden layer 2, relu activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-    # Output, linear activation
-    out = tf.matmul(layer_2, weights['out']) + biases['out']
-    return out
+from sklearn.grid_search import GridSearchCV
+from sklearn.tree import DecisionTreeRegressor
 
 # perform some sort of analysis on the crime_weather.csv data
 
 crime_weather = pd.read_csv('data/crime_weather.csv')
 
 crimes = crime_weather.columns.tolist()[12:]
-target = crime_weather['all-other-crimes']
+target = crime_weather[crimes]
 crime_weather.drop(crimes, axis=1, inplace=True)
-
-crimes = ['all-other-crimes']
 crime_weather.drop('Date', axis=1, inplace=True)
 crime_weather.fillna(0.0, inplace=True)
 
 today = []
-for i in xrange(1):
+for i in xrange(16):
     today.append([i, 15, 88, 8, 29.72, 74, 10, 24, 62, 6, 4])
 
 today = pd.DataFrame(today)
@@ -46,68 +32,22 @@ X_tr = scale.transform(X_tr)
 X_ts = scale.transform(X_ts)
 today = scale.transform(today)
 
-#X_tr.reset_index(drop=True)
-#y_tr.reset_index(drop=True)
-X_tr = pd.DataFrame(X_tr)
-y_tr = pd.DataFrame(y_tr)
+# perform grid search to optimize parameters
+reg = DecisionTreeRegressor(random_state=42, max_depth=7, min_samples_split=50)
+params = {}
+'''params = { 
+            'max_depth':[2, 5, 7, 10],
+            'min_samples_split':[20,30,50],
+            'max_features':[None]
+         }'''
+gscv = GridSearchCV(reg, params, cv=5)
 
-input_count = len(crime_weather.columns)
-hidden1 = 16
-hidden2 = 16
-output_count = 1#len(crimes)
+gscv.fit(X_tr, y_tr)
 
-X = tf.placeholder('float', [None, input_count])
-y = tf.placeholder('float', [None, output_count])
+print gscv.best_estimator_
 
-weights = {
-    'h1': tf.Variable(tf.random_normal([input_count, hidden1])),
-    'h2': tf.Variable(tf.random_normal([hidden1, hidden2])),
-    'out': tf.Variable(tf.random_normal([hidden2, output_count]))
-}
-
-biases = {
-    'b1': tf.Variable(tf.random_normal([hidden1])),
-    'b2': tf.Variable(tf.random_normal([hidden2])),
-    'out': tf.Variable(tf.random_normal([output_count]))
-}
-
-predictor = mlp(X, weights, biases)
-
-cost = tf.reduce_mean(tf.square(tf.sub(y, predictor)))
-optimize = tf.train.GradientDescentOptimizer(learning_rate=0.0001).minimize(cost)
-
-init = tf.initialize_all_variables()
-
-with tf.Session() as sess:
-    sess.run(init)
-
-    epoch_count = 100
-    batch_count = 50
-    for i in xrange(epoch_count):
-        average_cost = 0.0
-        kf = KFold(len(X_tr), n_folds=batch_count)
-        for train_index, test_index in kf:
-            _, c = sess.run([optimize, cost], feed_dict={   X: X_tr.iloc[train_index],
-                                                            y: y_tr.iloc[train_index]})
-
-            average_cost += c
-        print 'epoch {0}: {1}'.format(i, average_cost / batch_count)
-    
-    y_predicted = pd.DataFrame(sess.run(predictor, feed_dict={X:X_tr}), columns=crimes)
-    y_tspredicted = pd.DataFrame(sess.run(predictor, feed_dict={X:X_ts}), columns=crimes)
-    
-    train_score = sk.mean_squared_error(y_tr, y_predicted)
-    test_score = sk.mean_squared_error(y_ts, y_tspredicted)
-    print "Accuracy Train: ", train_score #accuracy.eval({X: X_tr, y: y_tr})
-    print "Accuracy Test:  ", test_score #accuracy.eval({X: X_ts, y: y_ts})
-    
-    today_predictions = sess.run(predictor, feed_dict={X:today})
-    today_predictions = pd.DataFrame(today_predictions, columns=crimes)
-    print today_predictions
-
-
-
-
-
-
-
+print 'TR Score: {0}'.format(gscv.score(X_tr, y_tr))
+print 'TS Score: {0}'.format(gscv.score(X_ts, y_ts))
+#print 'Today: {0}'.format(pd.DataFrame(gscv.predict(today), columns=crimes))
+#print pd.DataFrame(gscv.predict(X_ts)).describe()
+#print pd.DataFrame(y_ts).describe()
