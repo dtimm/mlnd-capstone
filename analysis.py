5cdf499c6bd0458d24772791a8c224afd77d650f
David Timm <timmmeister@gmail.com>
2016-06-16 21:03:22 -0600

    Implemented MLP

--- analysis.py
+++ analysis.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, KFold
 from sklearn.linear_model import Perceptron
 from sklearn.grid_search import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 
-from sklearn.pipeline import Pipeline
+import tensorflow as tf
+
+# Create model
+def mlp(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
 
 # perform some sort of analysis on the crime_weather.csv data
 
 crime_weather = pd.read_csv('data/crime_weather.csv')
-target = crime_weather['all-other-crimes']
 
 crimes = crime_weather.columns.tolist()[12:]
+target = crime_weather[crimes]
 crime_weather.drop(crimes, axis=1, inplace=True)
 crime_weather.drop('Date', axis=1, inplace=True)
 crime_weather.fillna(0.0, inplace=True)
@@ -33,17 +45,61 @@ X_tr = scale.transform(X_tr)
 X_ts = scale.transform(X_ts)
 today = scale.transform(today)
 
-#reg = BayesianRidge()
-#params = {}
-reg = Perceptron(class_weight='balanced')
-params = {'alpha':[1e-3,1e-2,1e-1,1.0], 'penalty':[None,'l1','l2','elasticnet']}
-gscv = GridSearchCV(reg, params)
+#X_tr.reset_index(drop=True)
+#y_tr.reset_index(drop=True)
+X_tr = pd.DataFrame(X_tr)
+y_tr = pd.DataFrame(y_tr)
 
-gscv.fit(X_tr, y_tr)
-print gscv.best_estimator_
+input_count = len(crime_weather.columns)
+hidden1 = 128
+hidden2 = 128
+output_count = len(crimes)
 
-print gscv.predict(today)
-predictions = gscv.predict(X_ts)
-print pd.Series(y_ts).describe(), pd.Series(predictions).describe()
-#print scale.inverse_transform(X_ts[predictions == 0])
-print gscv.score(X_tr, y_tr), gscv.score(X_ts, y_ts)
+X = tf.placeholder('float', [None, input_count])
+y = tf.placeholder('float', [None, output_count])
+
+weights = {
+    'h1': tf.Variable(tf.random_normal([input_count, hidden1])),
+    'h2': tf.Variable(tf.random_normal([hidden1, hidden2])),
+    'out': tf.Variable(tf.random_normal([hidden2, output_count]))
+}
+
+biases = {
+    'b1': tf.Variable(tf.random_normal([hidden1])),
+    'b2': tf.Variable(tf.random_normal([hidden2])),
+    'out': tf.Variable(tf.random_normal([output_count]))
+}
+
+predictor = mlp(X, weights, biases)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predictor, y))
+optimize = tf.train.AdamOptimizer(learning_rate=0.01).minimize(cost)
+
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    epoch_count = 10
+    batch_count = 10
+    for i in xrange(epoch_count):
+        average_cost = 0.0
+        kf = KFold(len(X_tr), n_folds=batch_count)
+        for train_index, test_index in kf:
+            _, c = sess.run([optimize, cost], feed_dict={   X: X_tr.iloc[train_index],
+                                                            y: y_tr.iloc[train_index]})
+
+            average_cost += c
+        print 'epoch {0}: {1}'.format(i, average_cost / batch_count)
+
+    correct_prediction = tf.equal(tf.argmax(predictor, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print "Accuracy Train: ", accuracy.eval({X: X_tr, y: y_tr})
+    print "Accuracy Test:  ", accuracy.eval({X: X_ts, y: y_ts})
+
+
+
+
+
+
+
